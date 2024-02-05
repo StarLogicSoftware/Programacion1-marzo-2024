52b93d44cff42b73c3aefd1e3c228de9cb2eb1ae
StarLogicSoftware <141787060+StarLogicSoftware@users.noreply.github.com>
2024-02-05 13:10:04 -0300

    V1

---
+++ Enunciados/V1/Program.cs
@@ -0,0 +1,12 @@
+
+// crear matriz de asientos
+string[,] asientos = new string[3,10];
+
+// cargamos la matriz con "L"
+for (int fila = 0; fila < 3; fila++)
+{
+	for (int columna = 0; columna < 10; columna++)
+	{
+        asientos[fila, columna] = "L";
+    }
+}
