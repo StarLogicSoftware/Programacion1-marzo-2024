aae90ecbcf652876d8ca45b17078485301d176fc
StarLogicSoftware <141787060+StarLogicSoftware@users.noreply.github.com>
2024-02-05 18:27:33 -0300

    Update Program.cs

--- Enunciados/V1/Program.cs
+++ Enunciados/V1/Program.cs
@@ -1,47 +1,48 @@
 
 // crear matriz de asientos
-string[,] asientos = new string[3,10];
+string[,] asientos = new string[3, 10];
 int filaReserva;
 int columnaReserva;
 
 // cargamos la matriz con "L"
 for (int fila = 0; fila < 3; fila++)
 {
-	for (int columna = 0; columna < 10; columna++)
-	{
+    for (int columna = 0; columna < 10; columna++)
+    {
         asientos[fila, columna] = "L";
     }
 }
 
-// Reserva de un asiento
-Console.Write("Fila a reservar: ");
-filaReserva = int.Parse(Console.ReadLine());
-
-Console.Write("Columna a reservar: ");
-columnaReserva = int.Parse(Console.ReadLine());
-
-// determinar si el asiento a reservar esta dentro de la matriz
-if ( filaReserva >= 1 && filaReserva <= 3 &&
-    columnaReserva >= 1 && columnaReserva <= 10 )
+do
 {
-    Console.WriteLine("El valor es valido");
-}
-else
-{
-    Console.WriteLine("El valor NO es valido");
-}
+    Console.Write("Fila a reservar: ");
+    filaReserva = int.Parse(Console.ReadLine());
+
+    Console.Write("Columna a reservar: ");
+    columnaReserva = int.Parse(Console.ReadLine());
+
+    if (filaReserva < 1 || filaReserva > 3 ||
+            columnaReserva < 1 || columnaReserva > 10)
+    {
+        Console.WriteLine("La fila/columna esta fuera del rango");
+    }
+
+} while (filaReserva < 1 || filaReserva > 3 ||
+            columnaReserva < 1 || columnaReserva > 10);
+
+
+
 
 // Verificamos que el asiento este libre antes de reservar
-if (asientos[--filaReserva, --columnaReserva] == "L" )
+if (asientos[--filaReserva, --columnaReserva] == "L")
 {
     // guardar una X en el lugar solicitado 
-    asientos[--filaReserva, --columnaReserva] = "X";
+    asientos[filaReserva, columnaReserva] = "X";
 }
 else // en cambio si el asiento ya esta reservado
 {
     Console.WriteLine("Ese asiento ya esta ocupado");
 }
-
 
 
 // mostrar todos los asientos
