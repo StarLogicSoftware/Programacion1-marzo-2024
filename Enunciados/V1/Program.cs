52655930bc37dee1d0a6823add516f68061adddd
StarLogicSoftware <141787060+StarLogicSoftware@users.noreply.github.com>
2024-02-05 16:42:14 -0300

    V1.1

--- Enunciados/V1/Program.cs
+++ Enunciados/V1/Program.cs
@@ -10,3 +10,15 @@ for (int fila = 0; fila < 3; fila++)
         asientos[fila, columna] = "L";
     }
 }
+
+// mostrar todos los asientos
+for (int fila = 0; fila < 3; fila++)
+{
+    for (int columna = 0; columna < 10; columna++)
+    {
+        //Console.Write( asientos[fila, columna] ); // forma facil
+        Console.Write($" {asientos[fila, columna]} "); // con interpolacion
+    }
+
+    Console.WriteLine();
+}
