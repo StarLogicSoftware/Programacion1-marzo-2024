752286ccb16b9bc4251a91c406747cc381b25118
Starlogic - Nicolás Fumo <starlogicsoftware@gmail.com>
2024-05-27 20:25:36 -0300

    enunciado problema lista de tareas

--- ListadoTareas/Program.cs
+++ ListadoTareas/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+        --- LISTADO DE TAREAS ---
+CREAR UN PROGRAMA PARA LISTA DE TAREAS QUE DEBE:
+    - Poder cargar tareas a realizar
+    - Debe cargar un titulo y un cuerpo o descripcion de la tarea
+    - Se pueden cargar, listar, ver y eliminar tareas
+    - Se debe usar un menu para las diferentes opciones
+PARTE 2 DEL PROGRAMA
+    - Poder editar una tarea
+    - Poder marcar tarea como completada
+    - Marcar con color rojo la terminada y la verde la sin terminar
+ */
+
