203943012b1b878be78b246497959054e1851090
Starlogic - Nicolás Fumo <starlogicsoftware@gmail.com>
2024-06-24 19:08:20 -0300

    Listas C#

--- Listas/Program.cs
+++ Listas/Program.cs
@@ -1,64 +1,71 @@
-﻿
+﻿// LISTAS DE OBJETOS
+List<Persona> clientes = new List<Persona>();
 
-// LISTAS -> LIST
-// LISTA ES COMO UN ARRAY PERO DE TAMAÑO DINAMICO (crece a medida que le
-// colocamos elementos en el interior)
+Persona per1 = new Persona();
+per1.Nombre = "Francisco";
+per1.Edad = 27;
 
+Persona per2 = new Persona();
+per2.Nombre = "Marcos";
+per2.Edad = 32;
 
-// declarar una lista
+Persona per3 = new Persona();
+per3.Nombre = "Marcos";
+per3.Edad = 40;
 
-//List<int> lista;
-//lista = new List<int>();
+clientes.Add(per1);
+clientes.Add(per2);
+clientes.Add(per3);
 
-List<int> lista = new List<int>();
-lista.Add(20); // cargo elementos dentro de la lista
-lista.Add(15);
-lista.Add(25); //
-lista.Add(21); // 
-lista.Add(3);
+// Find() Busca el primero que coincide con la busqueda                             
+Persona personaBuscada1 = clientes.Find(item => item.Nombre == "Marcos");
+//                                     funcion lambda (verdadero o falso)
 
-int valor = int.Parse(Console.ReadLine());
+// Find() Busca el ultimo que coincide con la busqueda  
+Persona personaBuscada2 = clientes.FindLast(item => item.Nombre == "Marcos");
 
-lista.Add(valor);
-//                   _________ saber cuantos elementos hay en la lista
-Console.WriteLine(lista.Count);
+// Busca todos los elementos que coinciden con la busqueda. DEVUELVE UNA LISTA
+List<Persona> personasBuscadas = clientes.FindAll(item => item.Nombre == "Marcos");
 
-lista.RemoveRange(2, 3); // borra desde ese indice la cantidad de elementos especificados
-
-lista.Remove(15); // Remover un elemento de adentro de la lista
-
-lista.Insert(1, 30); // inserta un elemento en el indice indicado, corriendo los demas
-
-lista.RemoveAt(2); // remueve un elemento en el indice especificado
-
-Console.WriteLine( lista.IndexOf(21) ); // obtener el indice de ese elemento
-
-//recorrer una lista (mostrar todos sus elementos)
-foreach (var item in lista)
+foreach (var item in personasBuscadas)
 {
-    Console.WriteLine(item);
+    Console.WriteLine(item.Edad);
 }
 
-
-
-
-
-
-
-
-//int[] dato;
-//dato = new int[5];
-
-//int[] dato = new int[5];
-//// variable [indice] = valor
-//     dato     [0]    =  25;
-
-//dato[1] = 0;
-//dato[2] = 12;
-//dato[3] = 9;
-//dato[4] = 17;
-
-//for (int contador = 0; contador < 5; contador++)
+// equivalente a Find()
+//foreach (var item in clientes)
 //{
-//    Console.WriteLine(dato[contador]);
+//    if(item.Nombre == "Francisco" && item.Edad > 20)
+//    {
+//        personaBuscada = item;
+//    }
 //}
+
+// Obtiene un cliente segun el indice con el que esta guardado en la lista
+//Persona personaDeLaLista = clientes[1];
+
+// Recorre una lista uno por uno, saca un cliente y muestra su nombre
+//for (int indice = 0; indice < clientes.Count; indice++)
+//{
+//    var persona = clientes[indice];
+
+//    Console.WriteLine($"Nombre: {persona.Nombre}");
+//}
+
+// Recorre una lista uno por uno, saca un cliente y muestra su nombre
+//foreach (var item in clientes)
+//{
+//    Console.WriteLine($"Nombre: {item.Nombre}");
+//}
+
+// Remueve un elemento de la lista
+//clientes.Remove(personaDeLaLista);
+
+// lista.Count se usa para obtener la cantidad de elementos guardados dentro de la lista
+//Console.WriteLine($"Cantidad de clientes: {clientes.Count}");
+
+class Persona
+{
+    public string Nombre;
+    public int Edad;
+}
